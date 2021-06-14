19ea94c8caea2915fdfee98ead43e2fc06103f70
MatTZer28 <qazwsx6024865@gmail.com>
2021-06-14 19:06:09 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,71 @@
--- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: javaclassproject2021
--- ------------------------------------------------------
--- Server version	8.0.24
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2021 at 01:05 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `javaclassproject2021`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `combine`
+--
+
+CREATE TABLE `combine` (
+  `CombineName` varchar(10) NOT NULL,
+  `CombinePrice` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `combine`
+--
+
+INSERT INTO `combine` (`CombineName`, `CombinePrice`) VALUES
+('漢堡', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `combineproduct`
+--
+
+CREATE TABLE `combineproduct` (
+  `CombineName` varchar(10) NOT NULL,
+  `ProductID` varchar(10) NOT NULL,
+  `Amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `combineproduct`
+--
+
+INSERT INTO `combineproduct` (`CombineName`, `ProductID`, `Amount`) VALUES
+('漢堡', 'MA', 5);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `companyinformation`
 --
 
-DROP TABLE IF EXISTS `companyinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `companyinformation` (
-  `ID` int unsigned NOT NULL,
+  `ID` int(10) UNSIGNED NOT NULL,
   `CompanyID` varchar(20) DEFAULT NULL,
   `ChineseName` varchar(20) DEFAULT NULL,
   `EnglishName` varchar(50) DEFAULT NULL,
@@ -34,28 +76,22 @@ CREATE TABLE `companyinformation` (
   `ChineseAddress` varchar(50) DEFAULT NULL,
   `EnglishAddress` varchar(100) DEFAULT NULL,
   `EmailAddress` varchar(50) DEFAULT NULL,
-  `WebSiteAddress` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `WebSiteAddress` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `companyinformation`
 --
 
-LOCK TABLES `companyinformation` WRITE;
-/*!40000 ALTER TABLE `companyinformation` DISABLE KEYS */;
-INSERT INTO `companyinformation` VALUES (1,'','','','','','','','','','','');
-/*!40000 ALTER TABLE `companyinformation` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `companyinformation` (`ID`, `CompanyID`, `ChineseName`, `EnglishName`, `Manager`, `UniNumber`, `PhoneNumber`, `FaxNumber`, `ChineseAddress`, `EnglishAddress`, `EmailAddress`, `WebSiteAddress`) VALUES
+(1, '', '', '', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `customerinformation`
 --
 
-DROP TABLE IF EXISTS `customerinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customerinformation` (
   `CustomerID` varchar(10) NOT NULL,
   `UniNumber` varchar(8) DEFAULT NULL,
@@ -69,27 +105,22 @@ CREATE TABLE `customerinformation` (
   `Address` varchar(50) DEFAULT NULL,
   `EmailAddress` varchar(50) DEFAULT NULL,
   `WebSiteAddress` varchar(200) DEFAULT NULL,
-  `Comment` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Comment` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customerinformation`
 --
 
-LOCK TABLES `customerinformation` WRITE;
-/*!40000 ALTER TABLE `customerinformation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customerinformation` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `customerinformation` (`CustomerID`, `UniNumber`, `Name`, `InvoiceHeader`, `Manager`, `PhoneNumber`, `PhoneExtention`, `FaxNumber`, `CellPhoneNumber`, `Address`, `EmailAddress`, `WebSiteAddress`, `Comment`) VALUES
+('001', '', '張博皓', '', '', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `empolyeeinformation`
 --
 
-DROP TABLE IF EXISTS `empolyeeinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `empolyeeinformation` (
   `EmpolyeeID` varchar(10) NOT NULL,
   `Name` varchar(20) DEFAULT NULL,
@@ -112,118 +143,119 @@ CREATE TABLE `empolyeeinformation` (
   `EmerRelation` varchar(10) DEFAULT NULL,
   `EmerPhone` varchar(20) DEFAULT NULL,
   `Comment` varchar(100) DEFAULT NULL,
-  `AvatarImage` mediumblob,
-  PRIMARY KEY (`EmpolyeeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `AvatarImage` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `empolyeeinformation`
---
-
-LOCK TABLES `empolyeeinformation` WRITE;
-/*!40000 ALTER TABLE `empolyeeinformation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `empolyeeinformation` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
   `ProductID` varchar(10) NOT NULL,
   `Name` varchar(20) DEFAULT NULL,
   `Specification` varchar(20) DEFAULT NULL,
   `Type` varchar(20) DEFAULT NULL,
   `Unit` varchar(5) DEFAULT NULL,
-  `Total` int unsigned DEFAULT NULL,
-  `Cost` int unsigned DEFAULT NULL,
-  `SellPrice` int unsigned DEFAULT NULL,
-  `SafeAmount` int unsigned DEFAULT NULL,
-  `VendorName` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Total` int(10) UNSIGNED DEFAULT NULL,
+  `Cost` int(10) UNSIGNED DEFAULT NULL,
+  `SellPrice` int(10) UNSIGNED DEFAULT NULL,
+  `SafeAmount` int(10) UNSIGNED DEFAULT NULL,
+  `VendorName` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES ('aa','','','','',50,0,0,0,''),('bb','你媽','','','',50,0,0,0,'生菜供應商');
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `product` (`ProductID`, `Name`, `Specification`, `Type`, `Unit`, `Total`, `Cost`, `SellPrice`, `SafeAmount`, `VendorName`) VALUES
+('MA', '紅茶', '', '', '', 0, 0, 0, 0, 'Matt');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `productstoreinwarehouse`
 --
 
-DROP TABLE IF EXISTS `productstoreinwarehouse`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productstoreinwarehouse` (
   `ProductID` varchar(10) NOT NULL,
   `WarehouseID` varchar(10) NOT NULL,
-  `Amount` int DEFAULT NULL,
-  PRIMARY KEY (`ProductID`,`WarehouseID`),
-  KEY `WarehouseID_idx` (`WarehouseID`) /*!80000 INVISIBLE */,
-  CONSTRAINT `ProductID` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `WarehouseID` FOREIGN KEY (`WarehouseID`) REFERENCES `warehouse` (`WarehouseID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `productstoreinwarehouse`
 --
 
-LOCK TABLES `productstoreinwarehouse` WRITE;
-/*!40000 ALTER TABLE `productstoreinwarehouse` DISABLE KEYS */;
-INSERT INTO `productstoreinwarehouse` VALUES ('aa','A002',50),('bb','A002',50);
-/*!40000 ALTER TABLE `productstoreinwarehouse` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `productstoreinwarehouse` (`ProductID`, `WarehouseID`, `Amount`) VALUES
+('MA', '1', 0);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `purchase`
 --
 
-DROP TABLE IF EXISTS `purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `purchase` (
   `PurchaseID` varchar(10) NOT NULL,
   `VendorID` varchar(10) DEFAULT NULL,
   `ProductID` varchar(10) NOT NULL,
   `WarehouseID` varchar(10) NOT NULL,
-  `CostPrice` int DEFAULT NULL,
-  `Amount` int DEFAULT NULL,
-  PRIMARY KEY (`PurchaseID`,`WarehouseID`,`ProductID`),
-  KEY `WarehouseID_idx` (`WarehouseID`),
-  KEY `ProductID_idx` (`ProductID`),
-  CONSTRAINT `ProductID1` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `WarehouseID1` FOREIGN KEY (`WarehouseID`) REFERENCES `warehouse` (`WarehouseID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `CostPrice` int(11) DEFAULT NULL,
+  `Amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `purchase`
 --
 
-LOCK TABLES `purchase` WRITE;
-/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `purchase` (`PurchaseID`, `VendorID`, `ProductID`, `WarehouseID`, `CostPrice`, `Amount`) VALUES
+('P001', 'V1', 'MA', '1', 0, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sell`
+--
+
+CREATE TABLE `sell` (
+  `SellID` varchar(10) NOT NULL,
+  `CustomerID` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sell`
+--
+
+INSERT INTO `sell` (`SellID`, `CustomerID`) VALUES
+('S001', '001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sellcombine`
+--
+
+CREATE TABLE `sellcombine` (
+  `SellID` varchar(10) NOT NULL,
+  `CombineName` varchar(10) NOT NULL,
+  `Amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sellcombine`
+--
+
+INSERT INTO `sellcombine` (`SellID`, `CombineName`, `Amount`) VALUES
+('S001', '漢堡', 10);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `vendorinformation`
 --
 
-DROP TABLE IF EXISTS `vendorinformation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vendorinformation` (
   `VendorID` varchar(10) NOT NULL,
   `UniNumber` varchar(8) DEFAULT NULL,
@@ -236,55 +268,147 @@ CREATE TABLE `vendorinformation` (
   `Address` varchar(50) DEFAULT NULL,
   `EmailAddress` varchar(50) DEFAULT NULL,
   `WebSiteAddress` varchar(200) DEFAULT NULL,
-  `Comment` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`VendorID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Comment` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `vendorinformation`
 --
 
-LOCK TABLES `vendorinformation` WRITE;
-/*!40000 ALTER TABLE `vendorinformation` DISABLE KEYS */;
-INSERT INTO `vendorinformation` VALUES ('001','','生菜供應商','','','','','','','','','');
-/*!40000 ALTER TABLE `vendorinformation` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `vendorinformation` (`VendorID`, `UniNumber`, `Name`, `InvoiceHeader`, `Manager`, `PhoneNumber`, `PhoneExtention`, `FaxNumber`, `Address`, `EmailAddress`, `WebSiteAddress`, `Comment`) VALUES
+('V1', '', 'Matt', '', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `warehouse`
 --
 
-DROP TABLE IF EXISTS `warehouse`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `warehouse` (
   `WarehouseID` varchar(10) NOT NULL,
   `Name` varchar(20) DEFAULT NULL,
   `Address` varchar(50) DEFAULT NULL,
   `PhoneNumber` varchar(20) DEFAULT NULL,
-  `FaxNumber` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`WarehouseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `FaxNumber` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `warehouse`
 --
 
-LOCK TABLES `warehouse` WRITE;
-/*!40000 ALTER TABLE `warehouse` DISABLE KEYS */;
-INSERT INTO `warehouse` VALUES ('A002','M2','','',''),('A003','M3','','','');
-/*!40000 ALTER TABLE `warehouse` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `warehouse` (`WarehouseID`, `Name`, `Address`, `PhoneNumber`, `FaxNumber`) VALUES
+('1', 'A', '', '', '');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `combine`
+--
+ALTER TABLE `combine`
+  ADD PRIMARY KEY (`CombineName`);
+
+--
+-- Indexes for table `combineproduct`
+--
+ALTER TABLE `combineproduct`
+  ADD PRIMARY KEY (`CombineName`,`ProductID`);
+
+--
+-- Indexes for table `companyinformation`
+--
+ALTER TABLE `companyinformation`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `customerinformation`
+--
+ALTER TABLE `customerinformation`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `empolyeeinformation`
+--
+ALTER TABLE `empolyeeinformation`
+  ADD PRIMARY KEY (`EmpolyeeID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indexes for table `productstoreinwarehouse`
+--
+ALTER TABLE `productstoreinwarehouse`
+  ADD PRIMARY KEY (`ProductID`,`WarehouseID`),
+  ADD KEY `WarehouseID_idx` (`WarehouseID`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`PurchaseID`,`WarehouseID`,`ProductID`),
+  ADD KEY `WarehouseID_idx` (`WarehouseID`),
+  ADD KEY `ProductID_idx` (`ProductID`);
+
+--
+-- Indexes for table `sell`
+--
+ALTER TABLE `sell`
+  ADD PRIMARY KEY (`SellID`);
+
+--
+-- Indexes for table `sellcombine`
+--
+ALTER TABLE `sellcombine`
+  ADD PRIMARY KEY (`SellID`,`CombineName`) USING BTREE;
+
+--
+-- Indexes for table `vendorinformation`
+--
+ALTER TABLE `vendorinformation`
+  ADD PRIMARY KEY (`VendorID`);
+
+--
+-- Indexes for table `warehouse`
+--
+ALTER TABLE `warehouse`
+  ADD PRIMARY KEY (`WarehouseID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `combineproduct`
+--
+ALTER TABLE `combineproduct`
+  ADD CONSTRAINT `NameConstraint` FOREIGN KEY (`CombineName`) REFERENCES `combine` (`CombineName`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `productstoreinwarehouse`
+--
+ALTER TABLE `productstoreinwarehouse`
+  ADD CONSTRAINT `ProductID` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `WarehouseID` FOREIGN KEY (`WarehouseID`) REFERENCES `warehouse` (`WarehouseID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD CONSTRAINT `ProductID1` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `WarehouseID1` FOREIGN KEY (`WarehouseID`) REFERENCES `warehouse` (`WarehouseID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sellcombine`
+--
+ALTER TABLE `sellcombine`
+  ADD CONSTRAINT `SellIDConstraint` FOREIGN KEY (`SellID`) REFERENCES `sell` (`SellID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-05-10 17:16:36
